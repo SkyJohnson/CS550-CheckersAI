36f19b83831b0d7f561e931ff411a8e6ffe25484
SkyJohnson <45185895+SkyJohnson@users.noreply.github.com>
2020-03-08 18:55:20 -0700

    Tested and fixed Alpha Beta Search

--- ai.py
+++ ai.py
@@ -44,12 +44,12 @@ class Strategy(abstractstrategy.Strategy):
 
     def utility(self, board, player):
         # Very basic utility function for testing purposes
-        minPlayer = board.other_player(player)
+        self.minplayer = board.other_player(player)
 
         countPlayerPawns = board.get_pawnsN()[board.playeridx(player)]
         countPlayerKings = board.get_kingsN()[board.playeridx(player)]
-        countOtherPawns = board.get_pawnsN()[board.playeridx(minPlayer)]
-        countOtherKings = board.get_kingsN()[board.playeridx(minPlayer)]
+        countOtherPawns = board.get_pawnsN()[board.playeridx(self.minplayer)]
+        countOtherKings = board.get_kingsN()[board.playeridx(self.minplayer)]
 
         return (countPlayerPawns - countOtherPawns) + (countPlayerKings - countOtherKings)
 
@@ -61,12 +61,12 @@ class Strategy(abstractstrategy.Strategy):
 
     def play(self, board):
         #Need to implement the alpha-beta search to finish this method
-        search = MinimaxAphaBetaSearch(self.maxPlayer, self.minPlayer, self.maxPlies)
+        search = MinimaxAlphaBetaSearch(self.maxplayer, self.minplayer, self.maxplies)
         # Search will return utility of best move
         # need to call board.move() and pass in the action with utility given from search
-        return search
+        return search.alphaBetaSearch(board)
 
-class MinimaxAphaBetaSearch:
+class MinimaxAlphaBetaSearch:
 # In this class, the alpha-beta pruning action shall take place with the functions
 # minValue and maxValue. They will be created according to the pseudocode on page 14, slide 28
 
@@ -81,14 +81,27 @@ class MinimaxAphaBetaSearch:
         depth = 0
         return self.maxValue(state, -1*math.inf, math.inf, self.maxPlies, depth)
 
+    def maxValue(self, state, alpha, beta, maxplies, depth): 
+            if state.is_terminal()[0] or depth >= maxplies:    # board is at a terminal state or we have reach max search depth
+                v = Strategy.utility(Strategy, state, self.maxPlayer)
+            else:
+                depth += 1
+                v = -1*math.inf
+                for action in state.get_actions(self.maxPlayer):
+                    v = max((v, self.minValue(state.move(action), alpha, beta, maxplies, depth)))
+                    if v >= beta:
+                        break
+                    else:
+                        alpha = max((alpha, v))
+            return v
     def minValue(self, state, alpha, beta, maxplies, depth):
-        if state.is_terminal() or depth >= maxplies:    # board is at a terminal state or we have reach max search depth
-            v = Strategy.utility(state, self.maxPlayer)
+        if state.is_terminal()[0] or depth >= maxplies:    # board is at a terminal state or we have reach max search depth
+            v = Strategy.utility(Strategy, state, self.minPlayer)
         else:
             depth += 1
             v = math.inf
-            for action in state.get_actions(self.maxPlayer):
-                v = min((v, maxValue(state.move(action), alpha, beta, maxplies, depth)))
+            for action in state.get_actions(self.minPlayer):
+                v = min((v, self.maxValue(state.move(action), alpha, beta, maxplies, depth)))
                 if v <= alpha:
                     break
                 else:
@@ -96,16 +109,4 @@ class MinimaxAphaBetaSearch:
         return v
 
 
-    def maxValue(self, state, alpha, beta, maxplies, depth): 
-        if state.is_terminal() or depth >= maxplies:    # board is at a terminal state or we have reach max search depth
-            v = Strategy.utility(state, self.maxPlayer)
-        else:
-            depth += 1
-            v = -1*math.inf
-            for action in state.get_actions(self.maxPlayer):
-                v = max((v, minValue(state.move(action), alpha, beta, maxplies, depth)))
-                if v >= beta:
-                    break
-                else:
-                   alpha = max((alpha, v))
-        return v
+    
